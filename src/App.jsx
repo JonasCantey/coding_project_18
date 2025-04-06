6436038f78504bb8881846d6435e7485aaff9f07
Jonas <jonashooncantey@gmail.com>
2025-04-06 19:17:19 -0400

    Task 1: Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greetings from "./components/Greetings";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to our store!</h1>
+      <Greetings name="Shalla Ablla" />
+    </div>
+  )
+}
+export default App;
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Greetings({name}) {
+    return (
+        <div>
+  <h2>Hello from the Greeting Component!</h2>
+  
+</div>
+    );
+}
+export default Greetings;
