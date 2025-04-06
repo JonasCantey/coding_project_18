e6d45adf7d9460a7320950d8b984741c2ebd5c4b
Jonas <jonashooncantey@gmail.com>
2025-04-06 19:18:16 -0400

    Task 2: Component within React

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -4,7 +4,7 @@ function Greetings({name}) {
     return (
         <div>
   <h2>Hello from the Greeting Component!</h2>
-  
+  <p>This is your first custom component in React.</p>
 </div>
     );
 }
